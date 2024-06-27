467e60dc16857640078f04c9fa7e9fb7e1ef8fbf
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2024-06-27 09:56:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ app.use(contactRouter)
 //  API Documentation tool
 app.use(
     treblle({
-      apiKey: tGMM21jm4cYeSi9ySK0Gtn1YcNivgz7m,
-      projectId: 83LQQqokF1SAoJhb,
+      apiKey: "tGMM21jm4cYeSi9ySK0Gtn1YcNivgz7m",
+      projectId: "83LQQqokF1SAoJhb",
       additionalFieldsToMask: [],
     })
   )
